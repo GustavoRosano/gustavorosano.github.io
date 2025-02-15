5c3c1952ffa0a4c5d8ed3747086682029db1743a
Gustavo <gurosano@gmail.com>
2025-02-15 18:41:36 -0300

    teste github pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/NOME-DO-REPO",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
